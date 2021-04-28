235475494de60da4ee7dc428b126995ced6bae64
PoorTable <PoorTable1337@gmail.com>
2021-04-28 14:52:14 +0300

    refactor

--- screens/Daily/DailyView.js
+++ screens/Daily/DailyView.js
@@ -1,97 +1,106 @@
 import React from "react";
 import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  FlatList,
+	SafeAreaView,
+	StyleSheet,
+	View,
+	ImageBackground,
+	FlatList,
 } from "react-native";
 import moment from "moment";
 
 import CityLine from "../../components/CityLine";
 import ModalActivityIndcator from "../../components/ModalActivityIndicator";
-import NoData from '../../components/NoData';
+import NoData from "../../components/NoData";
 
-const DailyView = ( props ) => {
-    const{
-        ps,
-        isLoading,
-        pTRHandler,
-        Cities,
-        getPerm,
-    }=props
-  return (
-    <SafeAreaView style={styles.fl}>
-      {isLoading ? (
-        <ModalActivityIndcator show={true} />
-      ) : ps ? (
-        <View>
-          <View style={styles.sr}>
-           
-            <FlatList
-              data={Cities}
-              renderItem={(itemData) => (
-                <CityLine
-                  name={moment(new Date(itemData.item.time)).format("MMMM, Do")}
-                  temp={itemData.item.temperature.day}
-                  wicon={itemData.item.wcondition}
-                  DH={true}
-                />
-              )}
-              refreshing={isLoading}
-              onRefresh={() => {
-                pTRHandler();
-              }}
-            />
-          </View>
-        </View>
-      ) : (
-        <NoData onPress={getPerm} />
-      )}
-    </SafeAreaView>
-  );
+const DailyView = (props) => {
+	const { ps, isLoading, pTRHandler, Cities, getPerm } = props;
+	return (
+		<SafeAreaView style={styles.fl}>
+			<ImageBackground
+				source={{
+					uri:
+						"https://t4.ftcdn.net/jpg/03/25/68/63/360_F_325686385_7xPlV6ZN5HMj15QJ9LRr8gOsk2nyP4pM.jpg",
+				}}
+				style={{ flex: 1 }}
+			>
+				{isLoading ? (
+					<ModalActivityIndcator show={true} />
+				) : ps ? (
+					<View>
+						<View style={styles.sr}>
+							<FlatList
+								data={Cities}
+								renderItem={(itemData) => (
+									<CityLine
+										name={moment(new Date(itemData.item.time)).format(
+											"MMMM, Do"
+										)}
+										temp={itemData.item.temperature.day}
+										wicon={itemData.item.wcondition}
+										DH={true}
+									/>
+								)}
+								refreshing={isLoading}
+								onRefresh={() => {
+									pTRHandler();
+								}}
+							/>
+						</View>
+					</View>
+				) : (
+					<NoData onPress={getPerm} />
+				)}
+			</ImageBackground>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  fl: {
-    flex: 1,
-    marginBottom: 15
-  },
-  Name: {
-    paddingVertical: 5,
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-  text: {
-    color: "#fff",
-    fontSize: 16,
-  },
-  access: {
-    backgroundColor: "#694fad",
-    marginVertical: 15,
-    height: 40,
-    width: 120,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 5,
-  },
-  descr: {
-    paddingVertical: 5,
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  sr: {
-    marginVertical: 10,
-    width: "100%",
-    marginStart: "5%",
-  },
-  header: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-  },
+	fl: {
+		flex: 1,
+		marginBottom: 15,
+	},
+	Name: {
+		paddingVertical: 5,
+		fontSize: 20,
+		fontFamily: "Roboto-LightItalic",
+		fontWeight: "bold",
+		color: black,
+	},
+	text: {
+		color: "#fff",
+		fontSize: 16,
+		fontFamily: "Roboto-LightItalic",
+		fontWeight: "bold",
+		color: black,
+	},
+	access: {
+		backgroundColor: "#694fad",
+		marginVertical: 15,
+		height: 40,
+		width: 120,
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: 5,
+	},
+	descr: {
+		paddingVertical: 5,
+	},
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	sr: {
+		marginVertical: 10,
+		width: "100%",
+		marginStart: "5%",
+	},
+	header: {
+		flex: 1,
+		justifyContent: "flex-start",
+		alignItems: "flex-start",
+	},
 });
 
 export default DailyView;
--- screens/Hourly/HourlyView.js
+++ screens/Hourly/HourlyView.js
@@ -1,115 +1,113 @@
 import moment from "moment";
 import React from "react";
 import {
-  FlatList, SafeAreaView,
-  StyleSheet,
-
-  Text,
-
-  View
+	FlatList,
+	SafeAreaView,
+	StyleSheet,
+	ImageBackground,
+	Text,
+	View,
 } from "react-native";
 import CityLine from "../../components/CityLine";
 import ModalActivityIndcator from "../../components/ModalActivityIndicator";
-import NoData from '../../components/NoData';
-
-
+import NoData from "../../components/NoData";
 
 const HourlyView = (props) => {
-    const{
-        ps,
-        isLoading,
-        pTRHandler,
-        Cities,
-        getPerm,
-        np
-    }=props
-    
-  return (
-    <SafeAreaView style={styles.fl}>
-      {isLoading ? (
-        <ModalActivityIndcator show={true} />
-      ) : ps ? (
-        <View style={styles.sr}>
-           <FlatList
-             data={Cities}
-             renderItem={(itemData) => (
-               <CityLine
-                 name={moment(new Date(itemData.item.time)).format("LT")}
-                 temp={itemData.item.temperature}
-                 wicon={itemData.item.wcondition}
-                 DH={true}
-               />
-             )}
-             refreshing={isLoading}
-             onRefresh={() => {
-               pTRHandler();
-             }}
-           />
-        </View>
-      ) : !np ? (
-        <NoData onPress={getPerm} />
-      ) : (
-        <View style={styles.container}>
-        <Image source={require("../../assets/NoData.png")} />
-        <Text style={styles.Name}>Cannot get an update</Text>
+	const { ps, isLoading, pTRHandler, Cities, getPerm, np } = props;
 
-        <Text style={styles.descr}>
-            Please check your connection to the internet
-        </Text>
-    </View>
-      )}
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView style={styles.fl}>
+			<ImageBackground
+				source={{
+					uri:
+						"https://t4.ftcdn.net/jpg/03/25/68/63/360_F_325686385_7xPlV6ZN5HMj15QJ9LRr8gOsk2nyP4pM.jpg",
+				}}
+				style={{ flex: 1 }}
+			>
+				{isLoading ? (
+					<ModalActivityIndcator show={true} />
+				) : ps ? (
+					<View style={styles.sr}>
+						<FlatList
+							data={Cities}
+							renderItem={(itemData) => (
+								<CityLine
+									name={moment(new Date(itemData.item.time)).format("LT")}
+									temp={itemData.item.temperature}
+									wicon={itemData.item.wcondition}
+									DH={true}
+								/>
+							)}
+							refreshing={isLoading}
+							onRefresh={() => {
+								pTRHandler();
+							}}
+						/>
+					</View>
+				) : !np ? (
+					<NoData onPress={getPerm} />
+				) : (
+					<View style={styles.container}>
+						<Image source={require("../../assets/NoData.png")} />
+						<Text style={styles.Name}>Cannot get an update</Text>
+
+						<Text style={styles.descr}>
+							Please check your connection to the internet
+						</Text>
+					</View>
+				)}
+			</ImageBackground>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  centred:{
-    width:'40%',
-  },
-  fl: {
-    flex: 1,
-    marginBottom: 15
-  },
-  Name: {
-    paddingVertical: 5,
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-  text: {
-    color: "#fff",
-    fontSize: 16,
-  },
-  access: {
-    backgroundColor: "#694fad",
-    marginVertical: 15,
-    height: 40,
-    width: 120,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 5,
-  },
-  descr: {
-    paddingVertical: 5,
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    
-  },
-  sr: {
-    flex:1,
-    marginVertical: 10,
-    width: "100%",
-    marginStart: "5%",
-  },
-  header: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-  },
+	centred: {
+		width: "40%",
+	},
+	fl: {
+		flex: 1,
+		marginBottom: 15,
+	},
+	Name: {
+		paddingVertical: 5,
+		fontFamily: "Roboto-LightItalic",
+		fontWeight: "bold",
+		color: black,
+		fontSize: 20,
+	},
+	text: {
+		color: "#fff",
+		fontSize: 16,
+	},
+	access: {
+		backgroundColor: "#694fad",
+		marginVertical: 15,
+		height: 40,
+		width: 120,
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: 5,
+	},
+	descr: {
+		paddingVertical: 5,
+	},
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	sr: {
+		flex: 1,
+		marginVertical: 10,
+		width: "100%",
+		marginStart: "5%",
+	},
+	header: {
+		flex: 1,
+		justifyContent: "flex-start",
+		alignItems: "flex-start",
+	},
 });
-
-
 
 export default HourlyView;
